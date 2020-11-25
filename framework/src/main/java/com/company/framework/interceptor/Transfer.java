5293354d0dd5fe6fbe1981fc097580be09e8126c
candi <candi@yearka.com>
2020-11-25 15:42:05 +0800

    完善

--- framework/src/main/java/com/company/framework/interceptor/Transfer.java
+++ framework/src/main/java/com/company/framework/interceptor/Transfer.java
@@ -15,16 +15,16 @@ public class Transfer {
 	// 请求参数
 	private RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
 	// MDC日志追踪ID
-	private String traceId = MdcUtil.get();
+//	private String traceId = MdcUtil.get();
 	// 其他传递数据
 
 	public void beforeCall() {
 		RequestContextHolder.setRequestAttributes(requestAttributes);
-		MdcUtil.put(traceId);
+//		MdcUtil.put(traceId);
 	}
 
 	public void afterCall() {
 		RequestContextHolder.resetRequestAttributes();
-		MdcUtil.remove();
+//		MdcUtil.remove();
 	}
 }
