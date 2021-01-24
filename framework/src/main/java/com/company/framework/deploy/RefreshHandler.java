3e7f7a88586e523160dce7a8fdd616a6e3fee2c5
candi <candi@yeahka.com>
2021-01-24 14:30:59 +0800

    优雅发版刷新ribbon服务列表，即时下线服务

--- framework/src/main/java/com/company/framework/deploy/RefreshHandler.java
+++ framework/src/main/java/com/company/framework/deploy/RefreshHandler.java
@@ -1,12 +1,16 @@
 package com.company.framework.deploy;
 
 import java.lang.reflect.Method;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 import com.company.framework.context.SpringContextUtil;
 import com.netflix.discovery.DiscoveryClient;
+import com.netflix.loadbalancer.DynamicServerListLoadBalancer;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,10 +23,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class RefreshHandler {
-	
+
 	@Autowired(required = false)
 	private MQAutoRefresh mqAutoRefresh;
-    
+
 	/**
 	 * 通知其他服务刷新注册服务列表
 	 */
@@ -33,7 +37,7 @@ public class RefreshHandler {
 		}
 		mqAutoRefresh.send(msg);
 	}
-    
+
 	/**
 	 * 刷新注册列表
 	 */
@@ -42,11 +46,39 @@ public class RefreshHandler {
 			Method method = DiscoveryClient.class.getDeclaredMethod("refreshRegistry");
 			method.setAccessible(true);
 			method.invoke(SpringContextUtil.getBean(DiscoveryClient.class));
-			log.info("refresh success!!!");
+			log.info("refresh registry success!!!");
+			refreshRibbon();
 			return true;
 		} catch (Exception e) {
-			log.error("refresh fail!!!", e);
+			log.error("refresh registry fail!!!", e);
 			return false;
+		}
+	}
+
+	/**
+	 * 刷新Ribbon列表
+	 */
+	public void refreshRibbon() {
+		try {
+			// 更新ribbon 的缓存
+			SpringClientFactory springClientFactory = SpringContextUtil.getBean("springClientFactory");
+			Method getContext = SpringClientFactory.class.getDeclaredMethod("getContext", String.class);
+			getContext.setAccessible(true);
+			Set<String> contextNames = springClientFactory.getContextNames();
+
+			for (String contextName : contextNames) {
+				AnnotationConfigApplicationContext context = (AnnotationConfigApplicationContext) getContext
+						.invoke(springClientFactory, contextName);
+				Object ribbonLoadBalancer = context.getBean("ribbonLoadBalancer");
+				Method updateListOfServers = DynamicServerListLoadBalancer.class
+						.getDeclaredMethod("updateListOfServers");
+				updateListOfServers.setAccessible(true);
+				updateListOfServers.invoke(ribbonLoadBalancer);
+				log.info("{} refresh success!!!", contextName);
+			}
+			log.info("refresh ribbon success!!!");
+		} catch (Exception e) {
+			log.error("refresh ribbon fail!!!", e);
 		}
 	}
 }
