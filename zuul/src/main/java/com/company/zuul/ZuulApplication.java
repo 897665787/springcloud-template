a69d9377381e4e03b2fb050b16f158dc1184cc1d
candi <candi@yeahka.com>
2021-12-05 23:05:30 +0800

    添加zuul

---
+++ zuul/src/main/java/com/company/zuul/ZuulApplication.java
@@ -0,0 +1,22 @@
+package com.company.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+import com.company.framework.context.SpringContextUtil;
+
+@EnableEurekaClient
+@SpringBootApplication(scanBasePackages = "com.company")
+@EnableZuulProxy
+public class ZuulApplication {
+	public static void main(String[] args) {
+		// SpringApplication.run(WebApplication.class, args);
+		
+		SpringApplication springApplication = new SpringApplication(ZuulApplication.class);
+		// 初始化ApplicationContext，保证在所有bean实例化前面
+		springApplication.addInitializers(SpringContextUtil.newInstance());
+		springApplication.run(args);
+	}
+}
