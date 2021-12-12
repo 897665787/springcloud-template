06cca169ebb6ed3d05e12f84c416f485e5f7eac4
candi <candi@yeahka.com>
2021-12-12 18:33:14 +0800

    修正header传递问题

--- framework/src/main/java/com/company/framework/interceptor/FeignHeaderInterceptor.java
+++ framework/src/main/java/com/company/framework/interceptor/FeignHeaderInterceptor.java
@@ -1,10 +1,14 @@
 package com.company.framework.interceptor;
 
+import java.util.Collection;
+import java.util.Map;
+
 import org.springframework.stereotype.Component;
 
 import com.company.framework.aspect.IdempotentUtil;
 import com.company.framework.context.HttpContextUtil;
 import com.company.framework.filter.MdcUtil;
+import com.google.common.collect.Maps;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
@@ -16,16 +20,21 @@ import feign.RequestTemplate;
  */
 @Component
 public class FeignHeaderInterceptor implements RequestInterceptor {
-	
+
 	@Override
 	public void apply(RequestTemplate template) {
+		Map<String, Collection<String>> headers = Maps.newHashMap();
+
 		// 当前登录用户相关headers
-		template.headers(HttpContextUtil.currentUserHeaders());
+		headers.putAll(HttpContextUtil.currentUserHeaders());
 		// 日志追踪ID
-		template.headers(MdcUtil.headers());
+		headers.putAll(MdcUtil.headers());
 		// 幂等
-		template.headers(IdempotentUtil.headers());
+		headers.putAll(IdempotentUtil.headers());
 		// 其他headers
-		// template.headers(headers);
+		// headers.putAll();
+		if (!headers.isEmpty()) {
+			template.headers(headers); // 如果集合为空，template.headers会清空header
+		}
 	}
 }
