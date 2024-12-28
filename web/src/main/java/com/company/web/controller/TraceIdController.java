3420e4de6849a2a37ac27de120b4fbf992891de8
JQ棣 <897665787@qq.com>
2024-12-28 13:52:46 +0800

    接入skywalking链路追踪

--- web/src/main/java/com/company/web/controller/TraceIdController.java
+++ web/src/main/java/com/company/web/controller/TraceIdController.java
@@ -1,19 +1,18 @@
 package com.company.web.controller;
 
-import java.util.List;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.stream.Collectors;
-
+import com.company.common.api.Result;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.company.common.api.Result;
-import com.google.common.collect.Lists;
-
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/traceid")
@@ -25,6 +24,19 @@ public class TraceIdController {
 
 	@Autowired(required = false)
 	private ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
+	@GetMapping(value = "/thread")
+	public Result<Integer> thread() {
+		log.info("log begin");
+		for (int i = 0; i < 5; i++) {
+			int n = i;
+			new Thread(() -> {
+				log.info("log in thread:{}", n);
+			}).start();
+		}
+		log.info("log end");
+		return Result.success(1);
+	}
 
 	@GetMapping(value = "/threadpool")
 	public Result<Integer> threadpool() {
@@ -57,13 +69,32 @@ public class TraceIdController {
 		List<Integer> list = Lists.newArrayList(1, 2, 3, 4, 5);
 		log.info("list:{}", list);
 
-		List<String> strList = list.parallelStream().map(v -> {
+		List<String> strList = list.parallelStream()
+				.map(v -> {
 			log.info("log in forkjoin:{}", v);
 			return v.toString();
 		}).collect(Collectors.toList());
 
 		log.info("strList:{}", strList);
 
+		return Result.success(1);
+	}
+
+	@GetMapping(value = "/completablefuture")
+	public Result<Integer> completablefuture() {
+		List<Integer> list = Lists.newArrayList(1, 2, 3, 4, 5);
+		log.info("list:{}", list);
+
+		List<CompletableFuture<String>> list2 = Lists.newArrayList();
+		for (int i = 0; i < list.size(); i++) {
+			final Integer v = list.get(i);
+			CompletableFuture<String> item = CompletableFuture.supplyAsync(() -> {
+				log.info("log in completablefuture:{}", v);
+				return v.toString();
+			});
+			list2.add(item);
+		}
+		CompletableFuture.allOf(list2.toArray(new CompletableFuture[0])).join();
 		return Result.success(1);
 	}
 }
