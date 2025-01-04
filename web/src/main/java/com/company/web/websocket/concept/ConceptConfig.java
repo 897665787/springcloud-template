eeb871123c080aa43cff86bf4e9204f765b66bbc
JQ棣 <897665787@qq.com>
2025-01-04 14:08:36 +0800

    websocket方案demo

---
+++ web/src/main/java/com/company/web/websocket/concept/ConceptConfig.java
@@ -0,0 +1,10 @@
+package com.company.web.websocket.concept;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.github.linyuzai.connection.loadbalance.websocket.EnableWebSocketLoadBalanceConcept;
+
+@Configuration
+@EnableWebSocketLoadBalanceConcept
+public class ConceptConfig {
+}
---
+++ web/src/main/java/com/company/web/websocket/concept/CustomWebSocketMessageHandler.java
@@ -0,0 +1,49 @@
+package com.company.web.websocket.concept;
+
+import java.util.Map;
+
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+import com.company.common.util.JsonUtil;
+import com.company.common.util.MdcUtil;
+import com.company.web.websocket.dto.WsMsg;
+import com.github.linyuzai.connection.loadbalance.core.concept.Connection;
+import com.github.linyuzai.connection.loadbalance.core.concept.ConnectionLoadBalanceConcept;
+import com.github.linyuzai.connection.loadbalance.core.extension.UserMessage;
+import com.github.linyuzai.connection.loadbalance.core.extension.UserSelector;
+import com.github.linyuzai.connection.loadbalance.core.message.Message;
+import com.github.linyuzai.connection.loadbalance.websocket.concept.WebSocketMessageHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class CustomWebSocketMessageHandler implements WebSocketMessageHandler {
+
+	@Override
+	public void onMessage(Message message, Connection connection, ConnectionLoadBalanceConcept concept) {
+		MdcUtil.put();
+		log.info("message:{}", JsonUtil.toJsonString(message));
+		Map<Object, Object> metadata = connection.getMetadata();
+		log.info("connection metadata:{}", JsonUtil.toJsonString(metadata));
+		
+		String userId = MapUtils.getString(metadata, UserSelector.KEY);
+		
+		String payload = message.getPayload();
+		// Object id = connection.getId();
+		
+		WsMsg wsMsg = JsonUtil.toEntity(payload, WsMsg.class);
+		String toUserId = wsMsg.getToUserId();
+		if (StringUtils.isBlank(toUserId)) {
+			String msg = String.format("接收到【%s】的信息:%s", userId, wsMsg.getMessage());
+			concept.send(msg);
+		} else {
+			String msg = String.format("接收到【%s】的信息:%s", userId, wsMsg.getMessage());
+			concept.send(new UserMessage(msg, toUserId));
+		}
+		MdcUtil.remove();
+	}
+
+}
