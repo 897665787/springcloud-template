8ecb3fc6bfb518eca0ed13846d04f5d12b1af6a4
JQ棣 <897665787@qq.com>
2025-01-09 20:27:27 +0800

    暂存

--- order/src/main/java/com/company/order/amqp/Constants.java
+++ order/src/main/java/com/company/order/amqp/Constants.java
@@ -6,38 +6,38 @@ public interface Constants {
 
 	interface EXCHANGE {
 		String DIRECT = PREFIX + "direct";
-		String XDELAYED = PREFIX + "x-delayed.direct";
+		String XDELAYED = PREFIX + "x-delayed-direct";
 	}
 
 	interface QUEUE {
 		// x-delayed 插件实现的延时队列
 		interface XDELAYED {
 			String NAME = PREFIX + "x-delayed";
-			String KEY = PREFIX + "routingkey.x-delayed";
+			String KEY = PREFIX + "key-x-delayed";
 		}
 
 		// x-dead-letter 2个队列实现的延时队列
 		interface DEAD_LETTER {
 			String NAME = PREFIX + "x-dead-letter";
-			String KEY = PREFIX + "routingkey.x-dead-letter";
+			String KEY = PREFIX + "key-x-dead-letter";
 		}
 
 		// 公共队列
 		interface COMMON {
 			String NAME = PREFIX + "common";
-			String KEY = PREFIX + "routingkey.common";
+			String KEY = PREFIX + "key-common";
 		}
 
 		// 秒杀队列
 		interface FLASH_KILL {
 			String NAME = PREFIX + "flash_kill";
-			String KEY = PREFIX + "routingkey.flash_kill";
+			String KEY = PREFIX + "key-flash_kill";
 		}
 		
 		// 支付回调队列
 		interface PAY_NOTIFY {
 			String NAME = PREFIX + "pay_notify";
-			String KEY = PREFIX + "routingkey.pay_notify";
+			String KEY = PREFIX + "key-pay_notify";
 		}
 	}
 }
--- tool/src/main/java/com/company/tool/amqp/Constants.java
+++ tool/src/main/java/com/company/tool/amqp/Constants.java
@@ -6,50 +6,50 @@ public interface Constants {
 
 	interface EXCHANGE {
 		String DIRECT = PREFIX + "direct";
-		String XDELAYED = PREFIX + "x-delayed.direct";
+		String XDELAYED = PREFIX + "x-delayed-direct";
 	}
 
 	interface QUEUE {
 		// x-delayed 插件实现的延时队列
 		interface XDELAYED {
 			String NAME = PREFIX + "x-delayed";
-			String KEY = PREFIX + "routingkey.x-delayed";
+			String KEY = PREFIX + "key-x-delayed";
 		}
 
 		// x-dead-letter 2个队列实现的延时队列
 		interface DEAD_LETTER {
 			String NAME = PREFIX + "x-dead-letter";
-			String KEY = PREFIX + "routingkey.x-dead-letter";
+			String KEY = PREFIX + "key-x-dead-letter";
 		}
 
 		// 公共队列
 		interface COMMON {
 			String NAME = PREFIX + "common";
-			String KEY = PREFIX + "routingkey.common";
+			String KEY = PREFIX + "key-common";
 		}
 
 		// 发送短信
 		interface SEND_SMS {
 			String NAME = PREFIX + "send_sms";
-			String KEY = PREFIX + "routingkey.send_sms";
+			String KEY = PREFIX + "key-send_sms";
 		}
 		
 		// 发送邮件
 		interface SEND_EMAIL {
 			String NAME = PREFIX + "send_email";
-			String KEY = PREFIX + "routingkey.send_email";
+			String KEY = PREFIX + "key-send_email";
 		}
 		
 		// 发送企微机器人信息
 		interface SEND_WEBHOOK {
 			String NAME = PREFIX + "send_webhook";
-			String KEY = PREFIX + "routingkey.send_webhook";
+			String KEY = PREFIX + "key-send_webhook";
 		}
 		
 		// 发送订阅消息
 		interface SEND_SUBSCRIBE {
 			String NAME = PREFIX + "send_subscribe";
-			String KEY = PREFIX + "routingkey.send_subscribe";
+			String KEY = PREFIX + "key-send_subscribe";
 		}
 
 	}
