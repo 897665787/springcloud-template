ed33d48525bceeb1b160199c7d3d7e4ca3123017
JQ棣 <897665787@qq.com>
2025-01-21 21:54:33 +0800

    暂存

---
+++ admin/src/main/java/com/company/admin/messagedriven/strategy/StrategyConstants.java
@@ -0,0 +1,7 @@
+package com.company.admin.messagedriven.strategy;
+
+public interface StrategyConstants {
+	String USER_STRATEGY = "userStrategy";
+	String MAP_STRATEGY = "mapStrategy";
+	String XDELAYMESSAGE_STRATEGY = "xDelayMessageStrategy";
+}
