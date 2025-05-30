95be23db2eb8ee7031dc0f5a8e046ad44012c1a6
JQ棣 <897665787@qq.com>
2025-05-30 14:52:25 +0800

    developer

--- gateway/src/main/java/com/company/gateway/developer/DeveloperRouteAutoConfiguration.java
+++ gateway/src/main/java/com/company/gateway/developer/DeveloperRouteAutoConfiguration.java
@@ -6,6 +6,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @LoadBalancerClients(defaultConfiguration = {DeveloperLoadBalancerConfiguration.class})
 public class DeveloperRouteAutoConfiguration {
-    public DeveloperRouteAutoConfiguration() {
-    }
 }
