4358fd43b2bdb5bff25231117f9c6de8eabfe2b0
JQ棣 <897665787@qq.com>
2025-06-12 10:02:15 +0800

    完善

--- tool-api/src/main/java/com/company/tool/api/feign/AppVersionFeign.java
+++ tool-api/src/main/java/com/company/tool/api/feign/AppVersionFeign.java
@@ -4,12 +4,9 @@ import com.company.common.api.Result;
 import com.company.tool.api.constant.Constants;
 import com.company.tool.api.feign.fallback.AppVersionFeignFallback;
 import com.company.tool.api.response.AppVersionCheckResp;
-import com.company.tool.api.response.BannerResp;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @FeignClient(value = Constants.FEIGNCLIENT_VALUE, path = "/appVersion", fallbackFactory = AppVersionFeignFallback.class)
 public interface AppVersionFeign {
