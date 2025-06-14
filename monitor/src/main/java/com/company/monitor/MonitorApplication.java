f924af0488f4086418fc28641065bbf15f99fd6e
JQ棣 <897665787@qq.com>
2025-06-15 00:32:09 +0800

    调整日志配置

--- monitor/src/main/java/com/company/monitor/MonitorApplication.java
+++ monitor/src/main/java/com/company/monitor/MonitorApplication.java
@@ -8,14 +8,9 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @EnableAdminServer
 @EnableEurekaClient
-@SpringBootApplication(scanBasePackages = "com.company")
+@SpringBootApplication
 public class MonitorApplication {
 	public static void main(String[] args) {
-		// SpringApplication.run(MonitorApplication.class, args);
-		
-		SpringApplication springApplication = new SpringApplication(MonitorApplication.class);
-		// 初始化ApplicationContext，保证在所有bean实例化前面
-		// springApplication.addInitializers(SpringContextUtil.newInstance());
-		springApplication.run(args);
+		SpringApplication.run(MonitorApplication.class, args);
 	}
 }
