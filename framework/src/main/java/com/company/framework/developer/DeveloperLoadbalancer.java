e546fe1e70004d36c5b3c0f9cbde65b51c21de54
JQ棣 <897665787@qq.com>
2025-07-01 12:16:02 +0800

    修改DeveloperLoadbalancer的逻辑，实现上更加接近RoundRobinLoadBalancer

--- framework/src/main/java/com/company/framework/developer/DeveloperLoadbalancer.java
+++ framework/src/main/java/com/company/framework/developer/DeveloperLoadbalancer.java
@@ -1,6 +1,7 @@
 package com.company.framework.developer;
 
 import com.company.framework.developer.policy.ServicePriorityPolicyManager;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,30 +10,37 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.*;
 import org.springframework.cloud.loadbalancer.core.NoopServiceInstanceListSupplier;
 import org.springframework.cloud.loadbalancer.core.RoundRobinLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.SelectedInstanceCallback;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import org.springframework.http.HttpHeaders;
 import reactor.core.publisher.Mono;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+/**
+ * 开发者负载均衡器，在RoundRobinLoadBalancer基础上添加了根据请求头过滤服务列表的逻辑
+ */
+@Slf4j
 public class DeveloperLoadbalancer extends RoundRobinLoadBalancer {
     private final Logger logger = LoggerFactory.getLogger(DeveloperLoadbalancer.class);
-    private final ObjectProvider<ServiceInstanceListSupplier> serviceInstanceListSupplierObjectProvider;
+    private final ObjectProvider<ServiceInstanceListSupplier> serviceInstanceListSupplierProvider;
     private final ServicePriorityPolicyManager servicePriorityPolicyManager;
     private final String developerHeaders;
+    private final AtomicInteger position;
+    private final String serviceId;
 
-    public DeveloperLoadbalancer(ObjectProvider<ServiceInstanceListSupplier> serviceInstanceListSupplierObjectProvider, String serviceId, ServicePriorityPolicyManager servicePriorityPolicyManager, String developerHeaders) {
-        super(serviceInstanceListSupplierObjectProvider, serviceId);
-        this.serviceInstanceListSupplierObjectProvider = serviceInstanceListSupplierObjectProvider;
+    public DeveloperLoadbalancer(ObjectProvider<ServiceInstanceListSupplier> serviceInstanceListSupplierProvider, String serviceId, ServicePriorityPolicyManager servicePriorityPolicyManager, String developerHeaders) {
+        super(serviceInstanceListSupplierProvider, serviceId);
+        this.serviceId = serviceId;
+        this.serviceInstanceListSupplierProvider = serviceInstanceListSupplierProvider;
+        this.position = new AtomicInteger(new Random().nextInt(1000));
         this.servicePriorityPolicyManager = servicePriorityPolicyManager;
         this.developerHeaders = developerHeaders;
     }
 
+    @Override
     public Mono<Response<ServiceInstance>> choose(Request request) {
         List<String> developerList;
         if (request instanceof DefaultRequest) {
@@ -47,24 +55,60 @@ public class DeveloperLoadbalancer extends RoundRobinLoadBalancer {
             developerList = null;
         }
 
-        ServiceInstanceListSupplier supplier = serviceInstanceListSupplierObjectProvider.getIfAvailable(NoopServiceInstanceListSupplier::new);
+        ServiceInstanceListSupplier supplier = serviceInstanceListSupplierProvider.getIfAvailable(NoopServiceInstanceListSupplier::new);
         return supplier.get(request).next().map(allServerList -> {
             Map<Integer, List<ServiceInstance>> serverInfoMap = allServerList.stream().collect(Collectors.groupingBy(v -> servicePriorityPolicyManager.serverOrder(v, developerList)));
             Optional<Integer> minOrder = serverInfoMap.keySet().stream().min(Integer::compareTo);
             List<ServiceInstance> serviceInstances = minOrder.map(serverInfoMap::get).orElse(allServerList);
-            ServiceInstance server = this.chooseServer(serviceInstances);
-            return server != null ? new DefaultResponse(server) : new EmptyResponse();
+
+            return processInstanceResponse(supplier, serviceInstances);
         }).doOnError((e) -> {
             this.logger.error("Developer filter ServiceInstance error :: " + e.getMessage(), e);
         }).onErrorReturn(new EmptyResponse());
     }
 
-    private ServiceInstance chooseServer(List<ServiceInstance> serverList) {
-        if (serverList != null && !serverList.isEmpty()) {
-            int size = serverList.size();
-            return serverList.get(ThreadLocalRandom.current().nextInt(size));
-        } else {
-            return null;
+    /**
+     * copy from RoundRobinLoadBalancer
+     *
+     * @param supplier
+     * @param serviceInstances
+     * @return
+     */
+    private Response<ServiceInstance> processInstanceResponse(ServiceInstanceListSupplier supplier,
+                                                              List<ServiceInstance> serviceInstances) {
+        Response<ServiceInstance> serviceInstanceResponse = getInstanceResponse(serviceInstances);
+        if (supplier instanceof SelectedInstanceCallback && serviceInstanceResponse.hasServer()) {
+            ((SelectedInstanceCallback) supplier).selectedServiceInstance(serviceInstanceResponse.getServer());
         }
+        return serviceInstanceResponse;
+    }
+
+    /**
+     * copy from RoundRobinLoadBalancer
+     *
+     * @param instances
+     * @return
+     */
+    private Response<ServiceInstance> getInstanceResponse(List<ServiceInstance> instances) {
+        if (instances.isEmpty()) {
+            if (log.isWarnEnabled()) {
+                log.warn("No servers available for service: " + serviceId);
+            }
+            return new EmptyResponse();
+        }
+
+        // Do not move position when there is only 1 instance, especially some suppliers
+        // have already filtered instances
+        if (instances.size() == 1) {
+            return new DefaultResponse(instances.get(0));
+        }
+
+        // Ignore the sign bit, this allows pos to loop sequentially from 0 to
+        // Integer.MAX_VALUE
+        int pos = this.position.incrementAndGet() & Integer.MAX_VALUE;
+
+        ServiceInstance instance = instances.get(pos % instances.size());
+
+        return new DefaultResponse(instance);
     }
 }
