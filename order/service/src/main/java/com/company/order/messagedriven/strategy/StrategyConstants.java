744f5a6befd11485ec90ce3d1d7cebd6825e4ffb
JQ棣 <897665787@qq.com>
2025-11-20 21:13:14 +0800

    暂存

--- order/service/src/main/java/com/company/order/messagedriven/strategy/StrategyConstants.java
+++ order/service/src/main/java/com/company/order/messagedriven/strategy/StrategyConstants.java
@@ -2,7 +2,6 @@ package com.company.order.messagedriven.strategy;
 
 public interface StrategyConstants {
 	String USER_STRATEGY = "userStrategy";
-	String MAP_STRATEGY = "mapStrategy";
 	String XDELAYMESSAGE_STRATEGY = "xDelayMessageStrategy";
 
 	String PAY_CLOSE_STRATEGY = "payCloseStrategy";
