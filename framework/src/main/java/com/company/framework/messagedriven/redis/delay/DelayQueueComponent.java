fa05be252c7e705e3595ae9bffa6dc8eeecf1720
JQ棣 <897665787@qq.com>
2025-11-22 23:23:36 +0800

    1

--- framework/src/main/java/com/company/framework/messagedriven/redis/delay/DelayQueueComponent.java
+++ framework/src/main/java/com/company/framework/messagedriven/redis/delay/DelayQueueComponent.java
@@ -1,5 +1,6 @@
 package com.company.framework.messagedriven.redis.delay;
 
+import cn.hutool.core.thread.ThreadUtil;
 import com.company.framework.lock.LockClient;
 import com.company.framework.messagedriven.MessageSender;
 import com.company.framework.messagedriven.constants.HeaderConstants;
@@ -29,10 +30,10 @@ public class DelayQueueComponent implements CommandLineRunner {
     private StringRedisTemplate stringRedisTemplate;
 
     @Autowired
-	private TraceManager traceManager;
+    private TraceManager traceManager;
     @Lazy
     @Autowired
-	private MessageSender messageSender;
+    private MessageSender messageSender;
     @Autowired
     private LockClient lockClient;
 
@@ -44,41 +45,44 @@ public class DelayQueueComponent implements CommandLineRunner {
         stringRedisTemplate.opsForZSet().add(DELAY_QUEUE, delayedJsonStr, executeTime);
     }
 
-	@Override
-	public void run(String... args) throws Exception {
-		Thread thread = new Thread(() -> {
-			while (true) {
-				try {
-                    long currentTime = System.currentTimeMillis();
-                    // 获取到期的消息（score小于等于当前时间）
-                    Set<String> delayedJsonStrSet = stringRedisTemplate.opsForZSet().rangeByScore(DELAY_QUEUE, 0, currentTime);
-                    if (delayedJsonStrSet == null || delayedJsonStrSet.isEmpty()) {
-                        Thread.sleep(1000);
-                        continue;
-                    }
-                    traceManager.put();
-                    for (String delayedJsonStr : delayedJsonStrSet) {
-                        DelayedConsumer delayedConsumer = JsonUtil.toEntity(delayedJsonStr, DelayedConsumer.class);
+    @Override
+    public void run(String... args) throws Exception {
+        Thread thread = new Thread(() -> {
+            while (true) {
+                try {
+                    lockClient.doInLock("lock:mq:delay", () -> {
+                        long currentTime = System.currentTimeMillis();
+                        // 获取到期的消息（score小于等于当前时间）
+                        Set<String> delayedJsonStrSet = stringRedisTemplate.opsForZSet().rangeByScore(DELAY_QUEUE, 0, currentTime);
+                        if (delayedJsonStrSet == null || delayedJsonStrSet.isEmpty()) {
+                            ThreadUtil.sleep(1000);
+                            return null;
+                        }
+                        traceManager.put();
+                        for (String delayedJsonStr : delayedJsonStrSet) {
+                            DelayedConsumer delayedConsumer = JsonUtil.toEntity(delayedJsonStr, DelayedConsumer.class);
 
-                        String messageJson = delayedConsumer.getMessageJson();
-                        Map<String, Object> messageMap = JsonUtil.toEntity(messageJson, Map.class);
+                            String messageJson = delayedConsumer.getMessageJson();
+                            Map<String, Object> messageMap = JsonUtil.toEntity(messageJson, Map.class);
 
-                        String exchange = delayedConsumer.getExchange();
-                        String routingKey = delayedConsumer.getRoutingKey();
-                        String strategyName = MapUtils.getString(messageMap, HeaderConstants.HEADER_STRATEGY_NAME);
-                        String body = MapUtils.getString(messageMap, "body");
-                        Map<String, Object> toJson = JsonUtil.toEntity(body, Map.class);
-                        messageSender.sendNormalMessage(strategyName, toJson, exchange, routingKey);
-                    }
-                    // 从延时队列中移除
-                    stringRedisTemplate.opsForZSet().remove(DELAY_QUEUE, delayedJsonStrSet);
-					traceManager.remove();
-				} catch (Exception e) {
-					log.error("DelayQueueComponent thread error", e);
-				}
-			}
-		});
-		thread.setDaemon(true);
-		thread.start();
-	}
+                            String exchange = delayedConsumer.getExchange();
+                            String routingKey = delayedConsumer.getRoutingKey();
+                            String strategyName = MapUtils.getString(messageMap, HeaderConstants.HEADER_STRATEGY_NAME);
+                            String body = MapUtils.getString(messageMap, "body");
+                            Map<String, Object> toJson = JsonUtil.toEntity(body, Map.class);
+                            messageSender.sendNormalMessage(strategyName, toJson, exchange, routingKey);
+                        }
+                        // 从延时队列中移除
+                        stringRedisTemplate.opsForZSet().remove(DELAY_QUEUE, delayedJsonStrSet);
+                        traceManager.remove();
+                        return null;
+                    });
+                } catch (Exception e) {
+                    log.error("rangeByScore thread error", e);
+                }
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
+    }
 }
