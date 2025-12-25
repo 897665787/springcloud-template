bac6678bb7fa1b31cf25c09e60bbabd76bab93b7
JQ棣 <897665787@qq.com>
2025-12-25 17:49:34 +0800

    暂存

--- framework-edge/src/main/java/com/company/framework/globalresponse/UnauthorizedException.java
+++ framework-edge/src/main/java/com/company/framework/globalresponse/UnauthorizedException.java
@@ -1,9 +1,12 @@
 package com.company.framework.globalresponse;
 
+import com.feiniaojin.gracefulresponse.api.ExceptionMapper;
+
 /**
  * 未授权异常
  *
  * @author JQ棣
  */
+@ExceptionMapper(code = "401", msg = "未授权，请登录")
 public class UnauthorizedException extends RuntimeException {
 }
